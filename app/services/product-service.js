97848d6dd65e371a1b946898d54c3e3cd0eff944
Thien <vanthien2k14@gmai.com>
2021-11-15 22:53:50 +0700

    test

--- app/services/product-service.js
+++ app/services/product-service.js
@@ -3,21 +3,21 @@ function ProductService() {
 
     this.getListProductApi = function() {
         return axios({
-            url:"https://618561dc23a2fe0017fff5e9.mockapi.io/api/products" ,
+            url:"https://61927954d3ae6d0017da811c.mockapi.io/api/products" ,
             method: "GET" ,
         });
     };
 
     this.deleteProductApi = function(id) {
         return axios({
-            url:`https://618561dc23a2fe0017fff5e9.mockapi.io/api/products/${id}`,
+            url:`https://61927954d3ae6d0017da811c.mockapi.io/api/products/${id}`,
             method:"DELETE"
         });
     };
 
     this.addProductApi = function(product) {
         return axios({
-            url : "https://618561dc23a2fe0017fff5e9.mockapi.io/api/products" ,
+            url : "https://61927954d3ae6d0017da811c.mockapi.io/api/products" ,
             method: "POST",
             // post phải có thêm data 
             data: product
@@ -26,13 +26,13 @@ function ProductService() {
 
     this.getProductById = function(id) {
         return axios({
-            url : `https://618561dc23a2fe0017fff5e9.mockapi.io/api/products/${id}` ,
+            url : `https://61927954d3ae6d0017da811c.mockapi.io/api/products/${id}` ,
             method: "GET",
         })
     }
     this.updateProductApi = function(product) {
         return axios({
-            url : `https://618561dc23a2fe0017fff5e9.mockapi.io/api/products/${product.id}` ,
+            url : `https://61927954d3ae6d0017da811c.mockapi.io/api/products/${product.id}` ,
             method: "PUT",
             data:product
         })
